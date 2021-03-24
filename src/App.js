3fc3db7be4cd628036c326170f6a9ed49a9113cb
Donald J Lancaster <35845538+dj443@users.noreply.github.com>
2021-03-23 22:09:12 -0500

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,35 @@
-import logo from './logo.svg';
+import { faSignInAlt } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import React from 'react';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header>
+        <nav className="navbar navbar-inverse">
+          <div className="container-fluid">
+            <div className="navbar-header">
+              <a className="navbar-brand" href="/">WebSiteName</a>
+            </div>
+            <ul className="nav navbar-nav">
+              <li className="active"><a href="/">Home</a></li>
+              <li><a href="/page1">Page 1</a></li>
+              <li><a href='/page2'>Page 2</a></li>
+            </ul>
+            <ul className="nav navbar-nav navbar-right">
+              <li><a href="signup"><span class="glyphicon glyphicon-user"></span> Sign Up</a></li>
+              <li><a href="login"><span class="glyphicon glyphicon-log-in"></span> Login</a></li>
+            </ul>
+          </div>
+        </nav>
       </header>
+      <section>
+
+      </section>
+      <footer>
+
+      </footer>
     </div>
   );
 }
